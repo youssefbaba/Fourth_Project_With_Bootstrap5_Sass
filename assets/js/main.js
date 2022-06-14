21fef08b61531b6c091119497fb6642309e5dad5
“youssefbaba” <ybaba398@gmail.com>
2022-06-14 22:38:25 +0100

    Third Commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,21 @@
 if (window.innerWidth > 992) {
   document.getElementById('carouselIndicatorsOne').dataset.bsInterval = false
+  document.getElementById('carouselIndicatorsTwo').dataset.bsInterval = false
 }
+
+
+let items = document.querySelectorAll('.testimonial .carousel .carousel-item')
+
+items.forEach((el) => {
+  const minPerSlide = 4
+  let next = el.nextElementSibling
+  for (var i = 1; i < minPerSlide; i++) {
+    if (!next) {
+      // wrap carousel by using first child
+      next = items[0]
+    }
+    let cloneChild = next.cloneNode(true)
+    el.appendChild(cloneChild.children[0])
+    next = next.nextElementSibling
+  }
+})
